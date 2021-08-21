fe349cdff70708837b48163e9109243b0438a4d0
turkeychikken <maxy.cao@gmail.com>
2021-08-20 21:52:25 -0700

    made some stuff work
    made the website return the name and level of a summoner on the NA server on league of legends through a query string

--- app.js
+++ app.js
@@ -3,20 +3,42 @@ const dotenv = require('dotenv')
 dotenv.config()
 const PORT = process.env.PORT
 
+const path = require('path')
+
 const express = require('express')
 const app = express()
+const api_key = "RGAPI-8be9c778-adcd-4aea-81e7-795bb64cf04d"
 
-app.get('/', (req, res)=>{
-    res.send("HELLO WORLD")
+const axios = require('axios')
+
+
+
+const getData = async(summonerName) => {
+    try{
+        let resp = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${api_key}`)
+        return resp.data
+    }
+    catch (error) {
+        console.log(error);
+        return
+    }
+
+}
+
+app.get('/lol/query/:name', (req, res)=>{
+    const {name} = req.params
+    async function run() {
+        let response = await getData(name)
+        if (response) {
+            res.send(JSON.stringify(`${response.name}: ${response.summonerLevel}`))
+            console.log(response)
+        }
+        else {
+            return res.status(200).json({ success: true, data: [] })
+        }
+    }
+    run()
 })
-
-
-
-
-
-
-
-
 
 
 app.listen(PORT, (req, res)=> {
