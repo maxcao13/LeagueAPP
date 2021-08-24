00f5db5e251bbd951af7e0195efabfad2b065a5e
turkeychikken <maxy.cao@gmail.com>
2021-08-23 22:53:08 -0700

    removed all the static assets from github
    removed some unneeded static assets from github and added some free champ rotation functionality on the main page

--- app.js
+++ app.js
@@ -11,11 +11,29 @@ const app = express()
 const api_key = "RGAPI-94d63258-447a-47b1-97b5-574bd7deead5"
 const { err_handle } = require("./public/methods")
 
+let champions = (require('./public/league_static/11.16.1/data/en_US/champion.json')).data;
+
 const axios = require('axios')
 
 app.use(express.static('./public'))
 app.set('views', './views')
 app.set('view engine', 'pug')
+
+function getChampionFromID(object, id) {
+    for (let [champ,data] of Object.entries(object)) {
+        if (Number(data.key) === id) {
+            return [champ]
+        } 
+    }
+}
+
+// function getPathFromChampion(champ) {
+//     return `/public/league_static/img/champion/tiles/${champ}_0`
+// }
+
+async function getFreeChamps(idArray) {
+    return idArray.map(x => getChampionFromID(champions, x))
+}
 
 async function getSummonerInfo(req, res, next) {
     try{
@@ -35,8 +53,7 @@ async function getSummonerInfo(req, res, next) {
 async function freeChamps(req, res, next) {
     try {
         let api_res = await axios.get(`https://na1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=${api_key}`)
-        req.body = api_res.data
-        console.log(req.body)
+        req.body = await getFreeChamps(api_res.data.freeChampionIds)
     }
     catch (e) {
         err_handle(e, res)
@@ -45,8 +62,9 @@ async function freeChamps(req, res, next) {
     next()
 }
 
+
 app.get('/', freeChamps, (req, res) => {
-    res.status(200).render('index', {list: req.body.freeChampionIds})
+    res.status(200).render('index', {list: req.body})
 })
 
 app.get('/lol/query', getSummonerInfo, (req, res)=>{
